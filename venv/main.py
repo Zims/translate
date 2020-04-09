aa7c666e09acef2ed83fbda9bc366c201a4d55e9
Zims <zims@inbox.lv>
2020-04-09 21:36:51 +0300

    finished

--- venv/main.py
+++ venv/main.py
@@ -6,4 +6,16 @@
 
 from googletrans import Translator
 
-print('ffs')
+with open('test.txt') as my_file:
+    long_string_list = my_file.readlines()
+long_string = ''.join(long_string_list)
+
+
+translator = Translator()
+
+
+translations = translator.translate(long_string, dest='lv')
+
+with open('translated.txt', mode='w') as my_tl:
+    text = my_tl.write(translations.text)
+
