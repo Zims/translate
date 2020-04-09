e4b96dcbcce3513953bbf06f77817963cd004f18
Zims <zims@inbox.lv>
2020-04-09 20:18:00 +0300

    initial

---
+++ venv/main.py
@@ -0,0 +1,9 @@
+# Translate file
+# I made a file on desktop in english. Read it with py. Store words in list?
+# Use module to translate it. Take the string list and combine it.
+# Output to file. Done!
+
+
+from googletrans import Translator
+
+print('ffs')
